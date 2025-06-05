663c34eec36732b8c827d7679eb5c44f449a0f5a
Rayyan Akhter <rayyanakhter1905@gmail.com>
2025-06-05 15:40:44 +0530

    task completed

--- src/app/api/pins/[boardId]/route.ts
+++ src/app/api/pins/[boardId]/route.ts
@@ -1,30 +1,31 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma as db } from "../../../../../lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 
-export async function GET(
-  _req: Request,
-  { params }: { params: { boardId: string } }
-) {
+// GET /api/pins/[boardId]
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+  const boardId = url.pathname.split("/").pop()!;
+
   const pins = await db.pin.findMany({
-    where: { boardId: params.boardId },
+    where: { boardId },
   });
 
   return NextResponse.json(pins);
 }
 
 // POST /api/pins/[boardId]
-export async function POST(
-  req: Request,
-  { params }: { params: { boardId: string } }
-) {
+export async function POST(req: NextRequest) {
+  const url = new URL(req.url);
+  const boardId = url.pathname.split("/").pop()!;
+
   const { userId } = await auth();
   const body = await req.json();
   const { x, y, text, authorName } = body;
 
   const newPin = await db.pin.create({
     data: {
-      boardId: params.boardId,
+      boardId,
       x,
       y,
       text,
